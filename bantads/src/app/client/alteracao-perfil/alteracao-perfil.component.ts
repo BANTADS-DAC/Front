988f5f9b24f57c35f5fbd8924687f63670736cd9
Arthur Gian <arthxr.ogian@gmail.com>
2024-03-18 11:52:42 -0300

    cliente-alteracao-perfil

---
+++ bantads/src/app/client/alteracao-perfil/alteracao-perfil.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-alteracao-perfil',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './alteracao-perfil.component.html',
+  styleUrl: './alteracao-perfil.component.css'
+})
+export class AlteracaoPerfilComponent {
+
+}
